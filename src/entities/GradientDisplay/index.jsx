bf9443d9cd1821adf69087e4f40de48b1fdcec80
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-05 15:43:56 -0500

    Add basic style to GradientDisplay component

--- src/entities/GradientDisplay/index.jsx
+++ src/entities/GradientDisplay/index.jsx
@@ -1,5 +1,7 @@
+import "./ui/styles.css"
+
 const GradientDisplay = () => {
-  return <section></section>
+  return <section className="gradient-display"></section>
 }
 
 export default GradientDisplay
