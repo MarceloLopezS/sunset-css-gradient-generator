37cf31fb78933d330f693c501c7f6d661b76f61e
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-13 19:00:43 -0500

    Decouple bussiness entity from extra UI

--- src/entities/GeneratedCode/index.jsx
+++ src/entities/GeneratedCode/index.jsx
@@ -3,16 +3,13 @@ import ClipboardSVG from "../../shared/ui/SVGs/Clipboard"
 
 const GeneratedCode = () => {
   return (
-    <section>
-      <p className="text-bold margin-block-end-50">Generated Code</p>
-      <div className={styles["code-container"]}>
-        <code></code>
-        <button className={styles["copy-to-clipboard"]}>
-          <span className="visually-hidden">Copy code to clipboard</span>
-          <ClipboardSVG />
-        </button>
-      </div>
-    </section>
+    <div className={styles["code-container"]}>
+      <code></code>
+      <button className={styles["copy-to-clipboard"]}>
+        <span className="visually-hidden">Copy code to clipboard</span>
+        <ClipboardSVG />
+      </button>
+    </div>
   )
 }
 
