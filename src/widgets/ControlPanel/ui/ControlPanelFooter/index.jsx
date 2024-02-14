e24794afb60f08943a6375c1507db76ade6e823c
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-14 18:19:44 -0500

    Update ControlPanel footer

--- src/widgets/ControlPanel/ui/ControlPanelFooter/index.jsx
+++ src/widgets/ControlPanel/ui/ControlPanelFooter/index.jsx
@@ -1,5 +1,29 @@
+import styles from "./ui/styles.module.css"
+import GithubSVG from "../../../../shared/ui/SVGs/Github"
+import AboutSVG from "../../../../shared/ui/SVGs/About"
+
 const ControlPanelFooter = () => {
-  return <section></section>
+  return (
+    <footer className={styles["contact-info"]}>
+      <span>Developed by:</span>
+      <a
+        href="https://github.com/MarceloLopezS"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <GithubSVG />
+        MarceloLopezS
+      </a>
+      <a
+        href="https://github.com/MarceloLopezS/CSS-gradient-generator"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AboutSVG />
+        About this project
+      </a>
+    </footer>
+  )
 }
 
 export default ControlPanelFooter
