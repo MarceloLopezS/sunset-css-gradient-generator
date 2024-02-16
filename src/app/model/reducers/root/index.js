49f7e05639d1afdab53d23a7fc583cf8c30733a8
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-16 13:29:54 -0500

    Persist theme value on localStorage

--- src/app/model/reducers/root/index.js
+++ src/app/model/reducers/root/index.js
@@ -1,9 +1,12 @@
 import { TOGGLE_THEME } from "../../../config/actions";
+import { setLocalStorageItem } from "../../../../shared/utils/functions"
 
 const rootReducer = (state = {}, action) => {
   switch (action.type) {
     case TOGGLE_THEME: {
       const theme = state.theme === "dark" ? "light" : "dark";
+
+      setLocalStorageItem("theme", theme)
 
       return { ...state, theme }
     }
