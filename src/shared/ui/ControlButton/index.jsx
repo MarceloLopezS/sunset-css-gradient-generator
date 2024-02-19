684c70d302864da79f327919687c50c16fb26a91
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-19 14:40:00 -0500

    Update allowed properties and styles

--- src/shared/ui/ControlButton/index.jsx
+++ src/shared/ui/ControlButton/index.jsx
@@ -1,8 +1,25 @@
 import styles from "./ui/styles.module.css"
 
-const ControlButton = ({ children }) => {
+const ControlButton = ({
+  children,
+  classList = [],
+  selected,
+  ...attributes
+}) => {
   return (
-    <button type="button" className={styles.btn}>
+    <button
+      type="button"
+      className={`${
+        classList.length > 0
+          ? classList.reduce(
+              (acc, className) => `${acc} ${className}`,
+              styles.btn
+            )
+          : styles.btn
+      }`}
+      data-selected={selected || null}
+      {...attributes}
+    >
       {children}
     </button>
   )
