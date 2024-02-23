954187d6f3229d3c5c815758e70a9b3b9d9f2ec9
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-23 16:52:25 -0500

    Add toSpaceSeparated function for strings

--- src/shared/utils/functions/index.js
+++ src/shared/utils/functions/index.js
@@ -23,5 +23,18 @@ export const capitalizeString = (string) => {
     throw new TypeError("String is expected to be capitalized.")
   }
 
-  return string[0].toUpperCase() + string.substring(1)
+  const strings = string.split(" ")
+  return strings.map(string => string[0].toUpperCase() + string.substring(1))
+    .join(" ")
+}
+
+export const toSpaceSeparated = (string, currentSeparator) => {
+  if (typeof string !== "string") {
+    throw new TypeError("String is expected to be separated.")
+  }
+  if (typeof currentSeparator !== "string") {
+    throw new TypeError("Separator to be replaced must be a string.")
+  }
+
+  return string.split(currentSeparator).join(" ")
 }
