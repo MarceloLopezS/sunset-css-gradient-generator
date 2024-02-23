26975b7dfbe76235b57407f36f2987f6094c34c3
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-23 17:06:13 -0500

    Add component for radial size control

---
+++ src/features/GradientRadialSizeControl/index.jsx
@@ -0,0 +1,51 @@
+import { useContext } from "react"
+import { StoreContext, StoreDispatchContext } from "../../shared/state/store"
+import {
+  CLOSEST_CORNER,
+  CLOSEST_SIDE,
+  FARTHEST_CORNER,
+  FARTHEST_SIDE,
+  RADIAL
+} from "../../shared/utils/constants"
+import {
+  capitalizeString,
+  toSpaceSeparated
+} from "../../shared/utils/functions"
+import { SET_RADIAL_SIZE } from "../../shared/state/config/actions"
+import ButtonGrid from "../../shared/ui/ButtonGrid"
+import ControlButton from "../../shared/ui/ControlButton"
+
+const availableSizes = [
+  CLOSEST_SIDE,
+  FARTHEST_SIDE,
+  CLOSEST_CORNER,
+  FARTHEST_CORNER
+]
+
+const GradientRadialSizeControl = () => {
+  const { gradientOptions } = useContext(StoreContext)
+  const dispatch = useContext(StoreDispatchContext)
+
+  const dispatchAction = (type, payload) => () => dispatch({ type, payload })
+
+  return (
+    <section>
+      <p className="text-bold margin-block-end-50">Size</p>
+      <ButtonGrid columns={2}>
+        {availableSizes.map(size => {
+          return (
+            <ControlButton
+              key={size}
+              selected={gradientOptions[RADIAL].size === size}
+              onClick={dispatchAction(SET_RADIAL_SIZE, { size })}
+            >
+              {capitalizeString(toSpaceSeparated(size, "-"))}
+            </ControlButton>
+          )
+        })}
+      </ButtonGrid>
+    </section>
+  )
+}
+
+export default GradientRadialSizeControl
