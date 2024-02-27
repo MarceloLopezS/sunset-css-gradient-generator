ddfa76423a7766ac47708a02a9afc3f6a388cf71
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-26 20:17:47 -0500

    Use store to read and set color format

--- src/features/OutputFormatControl/index.jsx
+++ src/features/OutputFormatControl/index.jsx
@@ -1,14 +1,30 @@
+import { useContext } from "react"
+import { StoreContext, StoreDispatchContext } from "../../shared/state/store"
+import { SET_COLOR_FORMAT } from "../../shared/state/config/actions"
+import { HEX, HSL, RGB } from "../../shared/utils/constants"
 import ButtonGrid from "../../shared/ui/ButtonGrid"
 import ControlButton from "../../shared/ui/ControlButton"
 
+const availableFormats = [HEX, RGB, HSL]
+
 const OutputFormatControl = () => {
+  const { gradientOptions } = useContext(StoreContext)
+  const dispatch = useContext(StoreDispatchContext)
+  const dispatchAction = (type, payload) => () => dispatch({ type, payload })
+
   return (
     <section>
       <p className="text-bold margin-block-end-50">Output Format</p>
       <ButtonGrid>
-        <ControlButton>Hex</ControlButton>
-        <ControlButton>RGB</ControlButton>
-        <ControlButton>HSL</ControlButton>
+        {availableFormats.map(format => (
+          <ControlButton
+            key={format}
+            selected={gradientOptions.colorFormat === format}
+            onClick={dispatchAction(SET_COLOR_FORMAT, { colorFormat: format })}
+          >
+            {format}
+          </ControlButton>
+        ))}
       </ButtonGrid>
     </section>
   )
