463e4a82103aeb42dea7b7e7f82e9705282fbf0c
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-02-27 17:02:15 -0500

    Set Store connection to generate gradient code
    Add code copy to clipboard functionallity.

--- src/entities/GeneratedCode/index.jsx
+++ src/entities/GeneratedCode/index.jsx
@@ -1,11 +1,45 @@
-import styles from "./ui/styles.module.css"
+import { useRef, useContext } from "react"
+import { StoreContext } from "../../shared/state/store"
 import ClipboardSVG from "../../shared/ui/SVGs/Clipboard"
+import {
+  pipe,
+  setGradientStyle,
+  appendString,
+  appendConicStartAngle,
+  appendRadialShape,
+  appendRadialSize,
+  appendGradientPosition,
+  appendLinearDirection,
+  appendGradientColors,
+  copyToClipboard
+} from "../../shared/utils/functions"
+import styles from "./ui/styles.module.css"
 
 const GeneratedCode = () => {
+  const codeRef = useRef(null)
+  const { gradientOptions } = useContext(StoreContext)
+  const generateGradient = pipe(
+    setGradientStyle(gradientOptions),
+    appendString("("),
+    appendConicStartAngle(gradientOptions),
+    appendRadialShape(gradientOptions),
+    appendRadialSize(gradientOptions),
+    appendGradientPosition(gradientOptions),
+    appendLinearDirection(gradientOptions),
+    appendString(", "),
+    appendGradientColors(gradientOptions),
+    appendString(")")
+  )
+
+  const onClipboardClick = () => copyToClipboard(codeRef.current.textContent)
+
   return (
     <div className={styles["code-container"]}>
-      <code></code>
-      <button className={styles["copy-to-clipboard"]}>
+      <code ref={codeRef}>{generateGradient()}</code>
+      <button
+        className={styles["copy-to-clipboard"]}
+        onClick={onClipboardClick}
+      >
         <span className="visually-hidden">Copy code to clipboard</span>
         <ClipboardSVG />
       </button>
