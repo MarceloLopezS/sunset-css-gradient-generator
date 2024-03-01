6920f927c204653a184b8e4369f7379b089b2ae4
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-03-01 11:40:39 -0500

    Optimize rendering
    Create ControlPanelRadialExtraProps component to
    localize state and prevent ControlPanel
    unnecessary re-renders.

---
+++ src/widgets/ControlPanel/ui/ControlPanelRadialExtraProps/index.jsx
@@ -0,0 +1,17 @@
+import GradientRadialShapeControl from "../../../../features/GradientRadialShapeControl"
+import GradientRadialSizeControl from "../../../../features/GradientRadialSizeControl"
+import { useStoreData } from "../../../../shared/state/store"
+import { RADIAL } from "../../../../shared/utils/constants"
+
+const ControlPanelRadialExtraProps = () => {
+  const style = useStoreData(state => state.gradientOptions.style)
+
+  return style === RADIAL ? (
+    <>
+      <GradientRadialShapeControl />
+      <GradientRadialSizeControl />
+    </>
+  ) : null
+}
+
+export default ControlPanelRadialExtraProps
