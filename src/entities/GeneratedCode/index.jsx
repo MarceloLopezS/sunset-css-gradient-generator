b6d9d6bdd23cafbf9d6b61a7ef76ad158f7a7533
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-03-05 17:39:38 -0500

    Add CopyToClipboard component to composition

--- src/entities/GeneratedCode/index.jsx
+++ src/entities/GeneratedCode/index.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react"
 import { useStoreData } from "../../shared/state/store"
-import ClipboardSVG from "../../shared/ui/SVGs/Clipboard"
 import {
   pipe,
   initGradientStyle,
@@ -10,15 +9,17 @@ import {
   appendRadialSize,
   appendGradientPosition,
   appendLinearDirection,
-  appendGradientColors,
-  copyToClipboard
+  appendGradientColors
 } from "../../shared/utils/functions"
 import styles from "./ui/styles.module.css"
+import CopyToClipboard from "../../shared/ui/CopyToClipboard"
 
 const GeneratedCode = () => {
   const codeRef = useRef(null)
   const gradientOptions = useStoreData(state => state.gradientOptions)
   const colorFormat = useStoreData(state => state.colorFormat)
+
+  const getCodeText = () => codeRef.current.textContent
 
   const generateGradient = pipe(
     initGradientStyle(gradientOptions),
@@ -33,18 +34,10 @@ const GeneratedCode = () => {
     appendString(")")
   )
 
-  const onClipboardClick = () => copyToClipboard(codeRef.current.textContent)
-
   return (
     <div className={styles["code-container"]}>
       <code ref={codeRef}>{generateGradient()}</code>
-      <button
-        className={styles["copy-to-clipboard"]}
-        onClick={onClipboardClick}
-      >
-        <span className="visually-hidden">Copy code to clipboard</span>
-        <ClipboardSVG />
-      </button>
+      <CopyToClipboard valueGetter={getCodeText} />
     </div>
   )
 }
