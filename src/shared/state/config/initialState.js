04b70f3b84ac9b8306e0ca150deb8127da706f80
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2024-03-08 15:46:08 -0500

    Update initial params
    Now params are taken from URL, but set a default value if no param is found.

--- src/shared/state/config/initialState.js
+++ src/shared/state/config/initialState.js
@@ -1,4 +1,4 @@
-import { getPreferedColorScheme } from "../../utils/functions"
+import { getAllURLSearchParams, getPreferedColorScheme, getURLSearchParam } from "../../utils/functions"
 import {
   SHOWN,
   INSIDE_CONTAINER,
@@ -13,25 +13,42 @@ import {
   HEX
 } from "../../utils/constants"
 
+const styleFromURL = getURLSearchParam("style")
+const linearDirectionFromUrl =
+  styleFromURL === LINEAR && getURLSearchParam("direction")
+const radialShapeFromUrl =
+  styleFromURL === RADIAL && getURLSearchParam("shape")
+const radialSizeFromUrl =
+  styleFromURL === RADIAL && getURLSearchParam("size")
+const radialPositionFromUrl =
+  styleFromURL === RADIAL && getURLSearchParam("position")
+const conicStartAngleFromUrl =
+  styleFromURL === CONIC && getURLSearchParam("startAngle")
+const colorsFromUrl = getAllURLSearchParams("color").map(colorString => {
+  const [id, value, stop] = colorString.split(" ")
+
+  return { id, value: `#${value}`, stop }
+})
+
 const initialState = {
   theme: getPreferedColorScheme(),
   panelDisplay: SHOWN,
   panelDisplayTogglerPosition: INSIDE_CONTAINER,
   gradientOptions: {
-    style: LINEAR,
+    style: styleFromURL || LINEAR,
     [LINEAR]: {
-      direction: BOTTOM_RIGHT,
+      direction: linearDirectionFromUrl || BOTTOM_RIGHT,
     },
     [RADIAL]: {
-      shape: CIRCLE,
-      size: FARTHEST_CORNER,
-      position: CENTER
+      shape: radialShapeFromUrl || CIRCLE,
+      size: radialSizeFromUrl || FARTHEST_CORNER,
+      position: radialPositionFromUrl || CENTER
     },
     [CONIC]: {
-      startAngle: DEGREES_0,
+      startAngle: conicStartAngleFromUrl || DEGREES_0,
       position: CENTER
     },
-    colors: [
+    colors: colorsFromUrl.length > 0 && colorsFromUrl || [
       { id: 1, value: "#00308f", stop: null },
       { id: 2, value: "#fe5810", stop: null }
     ]
